84b4f0a2cee363ae1a7eff2f9de9cb2471fde11e
mahipower003 <94957338+mahipower003@users.noreply.github.com>
2022-07-19 20:39:30 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (21.39%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.java
+++ 118-pascals-triangle/118-pascals-triangle.java
@@ -1,29 +1,27 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
         
-        
         List<List<Integer>> ans=new ArrayList<>();
-        int row=1;
-        
-        List<Integer> sublist=new ArrayList<>();
+        List<Integer> sublist= new ArrayList<>();
         sublist.add(1);
         ans.add(sublist);
-        
+        int row=1;
         
         while(row<numRows)
         {
             List<Integer> previousList=ans.get(row-1);
-            List<Integer> newSubList=new ArrayList<>();
+            List<Integer> newList=new ArrayList<>();
             
             for(int i=0;i<=row;i++)
             {
-                int leftelement= i>=1?previousList.get(i-1):0;
-                int rightelement = i<row?previousList.get(i):0;
-                newSubList.add(leftelement+rightelement);
+                int leftElement= i>=1? previousList.get(i-1):0;
+                int rightElement = i<row? previousList.get(i):0;
+                newList.add(leftElement+rightElement);
             }
             
-            ans.add(newSubList);
+            ans.add(newList);
             row++;
+        
         }
         
         return ans;
