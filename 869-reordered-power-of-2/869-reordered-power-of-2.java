02d1f8b792b8b24ea60e49cbc7f17850a1713ef7
mahipower003 <94957338+mahipower003@users.noreply.github.com>
2022-08-26 12:50:56 +0530

    Time: 1 ms (100.00%), Space: 41.3 MB (39.33%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.java
+++ 869-reordered-power-of-2/869-reordered-power-of-2.java
@@ -1,35 +1,42 @@
 public class Solution {
-    public boolean reorderedPowerOf2(int N) {
-        int[] digits = new int[10];
-        for (int i = N; i > 0; i /= 10) 
+    public static boolean reorderedPowerOf2(int n) {
+
+
+        int[] inputArr=new int[10];
+
+        for(int i=n;i>0;i=i/10)
         {
-            digits[i % 10]++;
+            inputArr[i%10]++;
         }
-        int num = 1;
-        int bits = 1;
-        while (bits < 32) {
-            if (isDigitsMatch(num, digits)) {
-                return true;
-            }
-            num =num*2;
-            ++bits;
+
+        int poweroftwo=1;
+        int digit=1;
+
+        while(digit<32)
+        {
+            if(comparesame(poweroftwo,inputArr)) return true;
+            poweroftwo*=2;
+            digit++;
         }
+
         return false;
     }
 
-    private boolean isDigitsMatch(int n, int[] digits) 
-    {        
-        int[] temp = new int[10];
-        for (int i = n; i > 0; i /= 10) 
+    private static boolean comparesame(int poweroftwo, int[] inputArr)
+    {
+        int[] poweroftwoarr=new int[10];
+        int start=0;
+
+        for(int i=poweroftwo;i>0;i=i/10)
         {
-            temp[i % 10]++;
+            poweroftwoarr[i%10]++;
         }
-        int j=0;
-        while(j<10)
+
+        for(int i=0;i<poweroftwoarr.length;i++)
         {
-            if(temp[j]!=digits[j]) return false;
-            j++;
+            if(poweroftwoarr[i]!=inputArr[i]) return false;
         }
+
         return true;
     }
 
